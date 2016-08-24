e47a77b5b8decde11db97bcda7c4bbc65d8f98f1
proCoder02 <ofc.amitpandey@outlook.com>
2016-08-24 12:42:13 +0530

    Add files via upload

---
+++ AppletSkel.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+import java.applet.*;
+
+/* 
+<applet code="AppletSkel" width=300 height=100>
+</applet>
+*/
+
+public class AppletSkel extends Applet
+{
+	public void init() 
+	{
+	}
+	public void start()
+	{
+	}
+	public void stop()
+ 	{
+	}
+	public void destroy()
+	{
+	}
+        public void paint(Graphics g)
+	{
+	}
+}	
